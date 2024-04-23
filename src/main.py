ec6b7a9dd704486ef6f34e96c21b90e4651ec95d
paintingofblue <90877067+paintingofblue@users.noreply.github.com>
2024-04-23 13:51:30 +1200

    Add wabt support

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ import string
 import hashlib
 import zipfile
 import subprocess
+import tarfile
+import shutil
 from logger import Logger
 from argparse import ArgumentParser
 from uwdtool import UWDTool
@@ -213,7 +215,69 @@ def run_cpp2il():
 		'--output-as', 'isil'
 	])
 
+def run_wasmtoolkit():
+	# check to see if wabt exists in the resources folder
+	os.makedirs("resources", exist_ok=True)
+	if not os.path.exists('resources/wabt'):
+		# fetch latest release
+		r = requests.get('https://api.github.com/repos/WebAssembly/wabt/releases/latest')
+		assets = r.json().get('assets', [])
+		if len(assets) == 0:
+			raise Exception("No assets found!")
 
+		# go through each asset and check if it matches the current system
+		system_name = 'ubuntu' if os.name == 'posix' else 'windows'
+		for asset in assets:
+			asset_name = asset.get('name', '')
+
+			# check to see if the asset is for the current system (and isn't a sha256 file)
+			if system_name in asset_name and '.sha256' not in asset_name:
+				asset_url = asset.get('browser_download_url')
+				asset_res = requests.get(asset_url)
+				with open('wabt.tar.gz', 'wb') as f:
+					f.write(asset_res.content)
+
+				# extract the tar.gz
+				try:
+					with tarfile.open('wabt.tar.gz', 'r:gz') as f:
+						f.extractall('resources/wabt/')
+				except tarfile.ReadError: # this just happens??? idfk why but it still extracts perfectly fine lol its so weird
+					pass
+
+				# move the bin folder to the root directory & remove redundant files
+				shutil.move('resources/wabt/wabt-1.0.34/bin', 'resources/')
+				shutil.rmtree('resources/wabt')
+
+				os.rename('resources/bin', 'resources/wabt')
+				os.remove('wabt.tar.gz')
+
+				# make all files in the wabt directory executable when on linux
+				if os.name == 'posix':
+					os.system('chmod +x resources/wabt/*')
+
+	# base state
+	wabt_args = [
+		f'{state["output_dir"]}/kour.wasm',
+		'--enable-all',
+	]
+
+	# run wasm2wat
+	logger.info('WABT: Running wasm2wat (this may take a while)')
+	wasm2wat_path = 'resources/wabt/wasm2wat' if os.name == 'posix' else 'resources/wabt/wasm2wat.exe'
+	subprocess.run([
+		wasm2wat_path,
+		*wabt_args,
+		'--output', f'{state["output_dir"]}/kour.wat'
+	])
+
+	# run wasm-decompile
+	logger.info('WABT: Running wasm-decompile (this may take a while)')
+	wasm_decompile_path = 'resources/wabt/wasm-decompile' if os.name == 'posix' else 'resources/wabt/wasm-decompile.exe'
+	subprocess.run([
+		wasm_decompile_path,
+		*wabt_args,
+		'--output', f'{state["output_dir"]}/kour.wasm.dcmp'
+	])
 
 def main():
 	parser = ArgumentParser(description="A simple program")
@@ -246,6 +310,7 @@ def main():
 	# extract data we want
 	extract_webdata(f'{state["output_dir"]}/kour.data')
 	run_cpp2il()
+	run_wasmtoolkit()
 
 if __name__ == "__main__":
 	main()
